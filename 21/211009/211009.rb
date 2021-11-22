e65ca5abf1dffac8160f79f557c3fe56a48b76ea
manman4 <7023566+manman4@users.noreply.github.com>
2021-11-22 12:04:08 +0900

    Stirling number

--- 21/211009/211009.rb
+++ 21/211009/211009.rb
@@ -2,18 +2,18 @@
 def stirling(n, k = 1)
   a = [1]
   p [0, a]
-  n.times{|i|
+  (1..n).each{|i|
     a << 0
     b = [0]
-    (0..i).each{|j|
+    (0..i - 1).each{|j|
       if k == 2
         b[j + 1] = a[j] + (j + 1) * a[j + 1]
       else
-        b[j + 1] = a[j] +       i * a[j + 1]
+        b[j + 1] = a[j] + (i - 1) * a[j + 1]
       end
     }
     a = b
-    p [i + 1, a]
+    p [i, a]
   }
 end
 
