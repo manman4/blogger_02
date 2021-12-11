d552a4dc1cc7b6b730f364caa35fef876916b828
manman4 <7023566+manman4@users.noreply.github.com>
2021-12-11 15:10:31 +0900

    ナゴヤ三角形について

---
+++ 21/211211/211211.rb
@@ -0,0 +1,19 @@
+def A(n)
+  ary = []
+  (1..n).each{|i|
+    (i + 1..n).each{|j|
+      if i.gcd(j) == 1 && (i - j) % 3 > 0
+        x, y, z = j * j, i * j, i * i
+        b = x + y + y
+        c = x + y + z 
+        a = y + y + z
+        ary << [    a, b, c]
+        ary << [b - a, b, c]
+      end
+    }
+  }
+  ary
+end
+
+n = 10
+A(n).sort.each{|i| p i}
