b5207f7842692c79cf2ce3239ef513540186f8ee
manman4 <7023566+manman4@users.noreply.github.com>
2025-05-02 22:19:45 +0900

    An identity motivated by an amazing identity of Ramanujan

---
+++ 25/250502/250502.rb
@@ -0,0 +1,63 @@
+# a(n) = 99*a(n-1) - 99*a(n-2) + a(n-3)
+def A(n, a, b, c)
+  ary = [a, b, c]
+  (3..n).each{|i| ary << 99*ary[-1] - 99*ary[-2] + ary[-3]}
+  ary
+end
+
+n = 10
+# A261004
+a_ary = A(n, -3, -461, -45343)
+# A269548
+b_ary = A(n, -1, -233, -22961)
+# A269549
+c_ary = A(n, 1, -199, -19799)
+# A269550
+d_ary = A(n, 7, 465, 45347)
+# A269551
+e_ary = A(n, 5, 237, 22965)
+# A269552
+f_ary = A(n, 3, 203, 19803)
+# A269553
+p_ary = A(n, -3, -435, -42763)
+# A269554
+q_ary = A(n, -1, -343, -33861)
+# A269555
+r_ary = A(n, 7, 439, 42767)
+# A269556
+s_ary = A(n, 5, 347, 33865)
+# A010701
+t_ary = [3] * (n + 1)
+
+a = [
+  a_ary,
+  b_ary,
+  c_ary,
+  d_ary,
+  e_ary,
+  f_ary,
+  p_ary,
+  q_ary,
+  r_ary,
+  s_ary,
+  t_ary
+]
+
+(0..n).each{|i|
+  (1..5).each{|j|
+    ary = (0..10).map{|k| a[k][i]}
+
+    terms = ary.each_with_index.map{|x, idx|
+      display = x < 0 ? "(#{x})^#{j}" : "#{x}^#{j}"
+      operator = idx == 0 ? "" : (idx < 6 ? " + " : " - ")
+      operator + display
+    }
+    expression = terms.join
+
+    # 実際の数値計算
+    sum = ary.each_with_index.map{|x, idx| idx < 6 ? x ** j : -x ** j}.sum
+
+    puts "#{expression} = #{sum}"
+  }
+  p ""
+}
